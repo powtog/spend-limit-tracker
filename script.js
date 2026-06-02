f2bf0bc1c3f72ffd2db7fa75efbf05210469c2ed
Randy Stormes <rstorm9@wgu.edu>
2026-06-02 18:02:11 -0400

    Display budget entered by user

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.querySelector("button").addEventListener("click", function () {
+    
+    const budget = document.querySelector("#budgetInput").value;
+    
     document.body.style.backgroundColor = "lightgreen";
+   
+    document.querySelector("#budgetDisplay").textContent = budget;
+
 });
