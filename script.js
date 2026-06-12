f54721fdba28b53612316257a9d2f1712d8d822f
Randy Stormes <rstorm9@wgu.edu>
2026-06-12 17:41:17 -0400

    Calculate remaining budget after charge

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.querySelector("button").addEventListener("click", function () {
     
-    const budget = document.querySelector("#budgetInput").value;
+    const budget = Number(document.querySelector("#budgetInput").value);
+
+    const charge = Number(document.querySelector("#chargeInput").value);
+
+    const remaining = budget - charge;
     
     document.body.style.backgroundColor = "lightgreen";
    
-    document.querySelector("#budgetDisplay").textContent = budget;
+    document.querySelector("#budgetDisplay").textContent =
+        "Remaining: $" + remaining.toFixed(2);
 
 });
