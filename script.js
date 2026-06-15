563922ab05e1216c36ef3f2a51deb920b97604e0
Randy Stormes <rstorm9@wgu.edu>
2026-06-15 13:06:40 -0400

    Persist budget and charges with localStorage

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-// empty array to hold each charge amount
-let charges = [];
+
+// search local storage for existing array else use an empty array
+let charges = JSON.parse(localStorage.getItem("charges")) || [];
+
+// set budget equal to remaining budget or 0
+let budget = Number(localStorage.getItem("budget")) || 0;
+
+// displays remaining budget on screen 
+function updateDisplay() {
+    const totalSpent = charges.reduce(function (sum, charge) {
+        return sum + charge;
+    }, 0);
+
+    const remaining = budget - totalSpent;
+
+    document.querySelector("#budgetDisplay").textContent = 
+        "Remaining: $" + remaining.toFixed(2);
+}
+// put saved budget back into the input box
+document.querySelector("#budgetInput").value = budget;
+
+// display saved remaining balance when page loads
+updateDisplay();
 
 document.querySelector("button").addEventListener("click", function () {
-    
-    const budget = Number(document.querySelector("#budgetInput").value);
-
+    // read budget input
+    budget = Number(document.querySelector("#budgetInput").value);
+    // read charge input
     const charge = Number(document.querySelector("#chargeInput").value);
 
     // add each charge to the list
     charges.push(charge);
 
-    // calculate the total spent
-    const totalSpent = charges.reduce(function (sum, charge) {
-        return sum + charge;
-    }, 0);
+    // save charges array and budget to local storage
+    localStorage.setItem("charges", JSON.stringify(charges));
+    localStorage.setItem("budget", budget);
 
-    // calculate the remaining budget by subtracting summed charges
-    const remaining = budget - totalSpent;
+    // update the screen
+    updateDisplay();
     
-    document.body.style.backgroundColor = "lightgreen";
    
-    document.querySelector("#budgetDisplay").textContent =
-        "Remaining: $" + remaining.toFixed(2);
-
 });
