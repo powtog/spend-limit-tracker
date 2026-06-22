f14669786142238933b35e8be7b67cdea5441eab
Randy Stormes <rstorm9@wgu.edu>
2026-06-22 10:04:58 -0400

    Add charge history list with show/hide toggle

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+// 
+let showCharges = false;
+
 // search local storage for existing array else use an empty array
 let charges = JSON.parse(localStorage.getItem("charges")) || [];
 
@@ -21,6 +24,23 @@ document.querySelector("#budgetInput").value = budget;
 
 // display saved remaining balance when page loads
 updateDisplay();
+
+// 
+function updateChargesList() {
+    const chargesList = document.querySelector("#chargesList");
+    // clear list
+    chargesList.textContent = "";
+    // loop through charges array
+    for (const charge of charges) {
+        // create <li>
+        const listItem = document.createElement("li");
+        // fill with date and amount
+        listItem.textContent = charge.date + " - $" + charge.amount.toFixed(2);
+        // append <ul>
+        chargesList.appendChild(listItem);
+    }
+
+}
 
 document.querySelector("button").addEventListener("click", function () {
     // read budget input
@@ -46,6 +66,28 @@ document.querySelector("button").addEventListener("click", function () {
 
     // update the screen
     updateDisplay();
-    
-   
+
+    // 
+    updateChargesList();
+
+});
+
+// uppon clicking "Show Charges" button:
+document.querySelector("#toggleChargesButton").addEventListener("click", function () {
+    // grab the charges list
+    const chargesList = document.querySelector("#chargesList");
+    // make show charges the opposite 
+    showCharges = !showCharges;
+    // build the charges list
+    updateChargesList();
+    // set the charges list hidden feature to T/F if show charges is F/T
+    chargesList.hidden = !showCharges;
+    // grab the show charges button
+    const toggleButton = document.querySelector("#toggleChargesButton"); 
+    // change the text if show charges is true or false/other
+    if (showCharges) {
+        toggleButton.textContent = "Hide Charges";
+    } else {
+        toggleButton.textContent = "Show Charges";
+    }
 });
