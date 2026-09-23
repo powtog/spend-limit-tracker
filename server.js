88a4446d4edfa3da39e2001997b44564d691280a
Randy Stormes <rstorm9@wgu.edu>
2026-09-23 16:03:06 -0400

    Add test API route

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const express = require("express");
 // Create an Express application.
 const app = express();
 
+// Respond to GET requests at /api/test.
+app.get("/api/test", function (req, res) {
+
+    // Send JSON back to the browser.
+    res.json({
+        message: "Backend is working!"
+    });
+});
+
 // Tell the application to listen on port 3000.
 app.listen(3000, function () {
 
