23db6c37ba270be43202e54fb563bcc0c6ff7100
Randy Stormes <rstorm9@wgu.edu>
2026-09-23 18:50:45 -0400

    Add transactions API route

--- server.js
+++ server.js
@@ -16,6 +16,44 @@ app.get("/api/test", function (req, res) {
     });
 });
 
+
+// Respond to GET requests at /api/transctions.
+app.get("/api/transactions", function (req, res) {
+
+    // Create fake transaction data to simulate transactions we will later receive from Plaid.
+    const transactions = [
+         {
+            amount: 22.99,
+            date: "2026-08-28"
+        },
+        {
+            amount: 17.50,
+            date: "2026-08-27"
+        },
+        {
+            amount: 13.67,
+            date: "2026-08-26"
+        },
+        {
+            amount: 12.50,
+            date: "2026-08-25"
+        },
+        {
+            amount: 42.19,
+            date: "2026-08-24"
+        },
+        {
+            amount: 7.25,
+            date: "2026-08-23"
+        }
+    ];
+
+    // Send the transaction array back to the frontend as JSON.
+    res.json(transactions);
+
+});
+
+
 // Tell the application to listen on port 3000.
 app.listen(3000, function () {
 
